f5833065a56f4f481c2d7befb64dab7332f7f335
Tristan Perez <31454023+TriscuitP@users.noreply.github.com>
2019-09-01 11:09:42 -0700

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -1,42 +1,32 @@
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.TreeMap;
-import java.util.TreeSet;
 
 public class Driver 
-	{
+{
 	
 	private static void traverseDirectory(Path path, InvertedIndex indexMap) throws IOException
-		{		
+	{		
 		if(Files.isDirectory(path))
-			{
+		{
 			try(DirectoryStream<Path> listing = Files.newDirectoryStream(path))
-				{
-				for (Path extension : listing)
-					{	
-					traverseDirectory(extension, indexMap);
-					}
-				}
-			}
-		else if(path.toString().toLowerCase().endsWith("html") || path.toString().endsWith("htm"))
 			{
-//			System.out.println("Found html file: " + path.toString());
-			InvertedIndexBuilder.buildIndex(path, indexMap);
+				for(Path extension : listing)	
+					traverseDirectory(extension, indexMap);
 			}
 		}
+		else if(path.toString().toLowerCase().endsWith("html") || path.toString().endsWith("htm"))
+		{
+			InvertedIndexBuilder.buildIndex(path, indexMap);
+		}
+	}
 
 	public static void main(String[] args) throws IOException
-		{
+	{
 		ArgumentMap commdLine = new ArgumentMap(args);
 		InvertedIndex indexMap = new InvertedIndex();	
-		
-		// Getting JSON file if commmand line contains '-index'
-		String jsonFile = commdLine.getString("-index", "index.json");
 		
 		// Building inverted index from files in subdirectory of the current working directory
 		if(commdLine.hasFlag("-path") && commdLine.hasValue("-path"))
@@ -44,8 +34,31 @@ public class Driver
 		
 		// Write that index as JSON
 		if(commdLine.hasFlag("-index"))
+		{
+			// Getting JSON file if command line contains '-index'
+			String jsonFile = commdLine.getString("-index", "index.json");
 			JSONWriter.asInvertedIndexObject(indexMap.getIndex(), Paths.get(jsonFile));
-		
 		}
-
+		
+		// Do query search, partial or exact
+		QuerySearch query = new QuerySearch(indexMap);
+		if(commdLine.hasFlag("-query") && commdLine.hasValue("-query"))
+		{
+			try
+			{
+				query.parseQuery(Paths.get(commdLine.getValue("-query")), commdLine.hasFlag("-exact"));
+			}
+			catch(IOException e)
+			{
+				System.out.println("Unable to read query file");
+			}
+		}
+		
+		// Search results
+		if(commdLine.hasFlag("-results"))
+		{
+			String searchResult = commdLine.getString("-results", "results.json");
+			query.toJSON(Paths.get(searchResult));			
+		}
 	}
+}
